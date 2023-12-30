39b1f4dd65094c8906372995956ff60e85e741df
Christian <ch.practiques@gmail.com>
2023-12-30 10:26:14 +0100

    feat(game): lock screen orientation android + base css frame + keys to env

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { createWeb3Modal, defaultWagmiConfig } from '@web3modal/wagmi'
 import { Web3Modal } from '@web3modal/wagmi/dist/types/src/client'
 import { mainnet, arbitrum, bsc } from 'viem/chains'
 import { watchAccount, disconnect, getAccount } from '@wagmi/core'
+import { ScreenOrientation } from '@capacitor/screen-orientation'
+import { environment } from 'src/environments/environment'
 
 @Component({
   selector: 'app-root',
@@ -10,19 +12,27 @@ import { watchAccount, disconnect, getAccount } from '@wagmi/core'
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  walletConnectProjectId = '01b4ad31e05b34f9bfc0afd40086547c'
-  infuraKey = '2f6db9819c0a49969a53cc41ca96a8b2'
-
   address: string | undefined
   modal: Web3Modal
 
   constructor() {
-    const projectId = this.walletConnectProjectId
+    this.lockOrientation()
+    this.initWalletConnect()
+  }
+
+  async lockOrientation() {
+    try {
+      await ScreenOrientation.lock({ orientation: 'landscape' })
+    } catch (error) {}
+  }
+
+  initWalletConnect() {
+    const projectId = environment.walletConnectProjectId
 
     const metadata = {
-      name: 'My Website',
-      description: 'My Website description',
-      url: 'https://mywebsite.com',
+      name: 'Uruk Bartas',
+      description: 'Play to earn game',
+      url: 'https://game.urukbartas.com/',
       icons: ['https://avatars.githubusercontent.com/u/89161645?s=400&u=45ee748438c04f06f854fc0d28942581967ef16f&v=4'],
     }
 
@@ -31,11 +41,6 @@ export class AppComponent {
 
     watchAccount((account) => (this.address = account.address))
 
-    // 3. Create modal
     this.modal = createWeb3Modal({ wagmiConfig, projectId, chains })
-  }
-
-  connectWallet() {
-    this.modal.open()
   }
 }
