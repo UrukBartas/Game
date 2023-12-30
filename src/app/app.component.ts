152409ac1c3edef9dbf779b73c7a1558ba4e94c5
Tihomir Stoychev Stoychev <tihomir.stoychev@mhp-net.de>
2023-12-30 16:46:25 +0100

    Implemented basic game frames and theming

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,6 @@
-import { Component } from '@angular/core'
-import { createWeb3Modal, defaultWagmiConfig } from '@web3modal/wagmi'
-import { Web3Modal } from '@web3modal/wagmi/dist/types/src/client'
-import { mainnet, arbitrum, bsc } from 'viem/chains'
-import { watchAccount, disconnect, getAccount } from '@wagmi/core'
-import { ScreenOrientation } from '@capacitor/screen-orientation'
-import { environment } from 'src/environments/environment'
+import { Component, inject } from '@angular/core';
+import { ScreenOrientation } from '@capacitor/screen-orientation';
+import { AccountService } from 'src/services/account.service';
 
 @Component({
   selector: 'app-root',
@@ -12,35 +8,15 @@ import { environment } from 'src/environments/environment'
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  address: string | undefined
-  modal: Web3Modal
-
+  public account = inject(AccountService);
   constructor() {
-    this.lockOrientation()
-    this.initWalletConnect()
+    this.lockOrientation();
+    this.account.initWalletConnect();
   }
 
   async lockOrientation() {
     try {
-      await ScreenOrientation.lock({ orientation: 'landscape' })
+      await ScreenOrientation.lock({ orientation: 'landscape' });
     } catch (error) {}
-  }
-
-  initWalletConnect() {
-    const projectId = environment.walletConnectProjectId
-
-    const metadata = {
-      name: 'Uruk Bartas',
-      description: 'Play to earn game',
-      url: 'https://game.urukbartas.com/',
-      icons: ['https://avatars.githubusercontent.com/u/89161645?s=400&u=45ee748438c04f06f854fc0d28942581967ef16f&v=4'],
-    }
-
-    const chains = [mainnet, arbitrum, bsc]
-    const wagmiConfig = defaultWagmiConfig({ chains, projectId, metadata })
-
-    watchAccount((account) => (this.address = account.address))
-
-    this.modal = createWeb3Modal({ wagmiConfig, projectId, chains })
   }
 }
---
+++ src/modules/game/activities/character/character.component.ts
@@ -0,0 +1,11 @@
+import { Component, inject } from '@angular/core';
+import { AccountService } from 'src/services/account.service';
+
+@Component({
+  selector: 'app-character',
+  templateUrl: './character.component.html',
+  styleUrl: './character.component.scss',
+})
+export class CharacterComponent {
+
+}
