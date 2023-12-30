30a1fd59ec8431db3681ad085a58394533ec45db
Christian <ch.practiques@gmail.com>
2023-12-30 12:16:28 +0100

    fix(build): env

---
+++ src/environments/environment.ts
@@ -0,0 +1,18 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  walletConnectProjectId: '01b4ad31e05b34f9bfc0afd40086547c',
+  infuraKey: '2f6db9819c0a49969a53cc41ca96a8b2',
+}
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
