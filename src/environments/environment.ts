5b847c3b6f18e1e4b7a136ba5a656c9baf8411f1
Christian <ch.practiques@gmail.com>
2023-12-31 13:25:53 +0100

    feat(project): add dotenv + prettier

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,7 @@
 
 export const environment = {
   production: false,
-  walletConnectProjectId: '01b4ad31e05b34f9bfc0afd40086547c',
-  infuraKey: '2f6db9819c0a49969a53cc41ca96a8b2',
+  apiUrl: 'http://localhost:3000',
 }
 
 /*
