bb92d856a90ce65f5f001eda080d3c2686ee642a
Tihomir Stoychev Stoychev <tihomir.stoychev@mhp-net.de>
2024-01-29 12:41:36 +0100

    Checkpoint export/import

--- src/services/contract.service.ts
+++ src/services/contract.service.ts
@@ -1,25 +1,40 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
 import { readContract, writeContract } from '@wagmi/core';
 import UrukNFTArtifact from '../assets/UrukNFT.json';
+import { ApiBaseService } from 'src/modules/core/services/api-base.service';
+import { HttpClient } from '@angular/common/http';
+import { HelmetModel } from 'src/modules/core/models/items.model';
 @Injectable({
   providedIn: 'root',
 })
-export class ContractService {
-  constructor() {}
+export class ContractService extends ApiBaseService {
+  constructor(private http: HttpClient) {
+    super(http);
+    this.controllerPrefix = '/import-export';
+  }
 
   public executeReadContractOnUrukNFT(functionName: string) {
     return readContract({
-      address: process.env['CONTRACT_ADDRESS'] as any,
+      address: process.env['CONTRACT_ADDRESS_URUK'] as any,
       abi: UrukNFTArtifact.abi,
       functionName,
     });
   }
 
-  public executewriteContractOnUrukNFT(functionName: string) {
+  public executewriteContractOnUrukNFT(functionName: string, args: Array<any>) {
     return writeContract({
-      address: process.env['CONTRACT_ADDRESS'] as any,
+      address: process.env['CONTRACT_ADDRESS_URUK'] as any,
       abi: UrukNFTArtifact.abi,
       functionName,
+      args,
     });
+  }
+
+  public whiteListItem(itemId: string, address: string) {
+    return this.post('/whitelist-export/' + address + '/' + itemId, {});
+  }
+
+  public uploadJsonMetadataNFT(item: HelmetModel) {
+    return this.post('/upload-metadata', item);
   }
 }
