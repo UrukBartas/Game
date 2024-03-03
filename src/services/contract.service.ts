7c6ed7f0803194911312eee0231c1b90c2906875
Tihomir Stoychev Stoychev <tihomir.stoychev@mhp-net.de>
2024-03-03 10:51:08 +0100

    Arreglado visualización de stats, escondido campo email cuando se ve a alguien en el leaderboard, añadido redirects para netlify

--- src/services/contract.service.ts
+++ src/services/contract.service.ts
@@ -2,13 +2,11 @@ import { Inject, Injectable } from '@angular/core';
 import { readContract, watchContractEvent, writeContract } from '@wagmi/core';
 import UrukNFTArtifact from '../assets/UrukNFT.json';
 import GoldenUruks from '../assets/GoldenUruks.json';
-import { ApiBaseService } from 'src/modules/core/services/api-base.service';
-import { HttpClient } from '@angular/common/http';
 import { Contract, ethers } from 'ethers';
 @Injectable({
   providedIn: 'root',
 })
-export class ContractService extends ApiBaseService {
+export class ContractService {
   public provider = new ethers.JsonRpcProvider(process.env['PROVIDER']);
   adminWallet = new ethers.Wallet(process.env['PRIVATE_KEY'], this.provider);
   accountAWallet = new ethers.Wallet(
@@ -27,11 +25,6 @@ export class ContractService extends ApiBaseService {
     GoldenUruks.abi,
     this.adminWallet
   ) as Contract;
-
-  constructor(private http: HttpClient) {
-    super(http);
-    this.controllerPrefix = '/import-export';
-  }
 
   public executeReadContractOnUrukNFT(functionName: string, args: Array<any>) {
     return readContract({
