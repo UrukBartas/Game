cd45467cc709a71b6476bcf112d939207f47e6e1
Christian Moltó <cmolto@azigrene.es>
2024-07-03 22:32:28 +0200

    feat(login): new portal login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule, Type } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegisterReferralComponent } from 'src/modules/game/activities/campaigns/register-referral/register-referral.component';
+import { ConnectComponent } from 'src/modules/game/activities/connect/connect.component';
 import { GameModule } from 'src/modules/game/game.module';
 
 const routes: Routes = [
+  {
+    path: '',
+    component: ConnectComponent,
+    title: 'Login',
+  },
   {
     path: '',
     loadChildren: (): Promise<Type<GameModule>> =>
