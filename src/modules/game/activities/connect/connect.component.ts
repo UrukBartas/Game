805cb30e23cfe3ea9d0c62d16de3705573e400fc
Christian Moltó <cmolto@azigrene.es>
2024-07-03 22:32:28 +0200

    feat(login): new portal login

--- src/modules/game/activities/connect/connect.component.ts
+++ src/modules/game/activities/connect/connect.component.ts
@@ -1,28 +1,39 @@
-import { Component, inject, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  inject,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { Capacitor } from '@capacitor/core';
 import { Store } from '@ngxs/store';
+import { ToastrService } from 'ngx-toastr';
 import { TemplatePage } from 'src/modules/core/components/template-page.component';
 import { AuthService } from 'src/services/auth.service';
 import { WalletService } from 'src/services/wallet.service';
 import { LoginPlayer, MainState } from 'src/store/main.store';
-import { Location } from '@angular/common';
-import { firstValueFrom } from 'rxjs';
-import { ToastrService } from 'ngx-toastr';
+import { ThreePortalService } from './service/three-portal.service';
 @Component({
   selector: 'app-connect',
   templateUrl: './connect.component.html',
   styleUrls: ['./connect.component.scss'],
 })
-export class ConnectComponent extends TemplatePage implements OnInit {
+export class ConnectComponent
+  extends TemplatePage
+  implements OnInit, AfterViewInit
+{
+  @ViewChild('threeContainer', { static: true })
+  threeContainer!: ElementRef<HTMLDivElement>;
+
   walletService = inject(WalletService);
   authService = inject(AuthService);
   toast = inject(ToastrService);
   store = inject(Store);
   router = inject(Router);
   fb = inject(FormBuilder);
-
+  threeService = inject(ThreePortalService);
   public doingLogin = false;
   public formGroup = this.fb.group({
     email: ['', [Validators.required, Validators.email]],
@@ -34,6 +45,10 @@ export class ConnectComponent extends TemplatePage implements OnInit {
     if (player) {
       this.router.navigateByUrl('/inventory');
     }
+  }
+
+  ngAfterViewInit(): void {
+    this.threeService.initialize(this.threeContainer);
   }
 
   public async doLogin() {
