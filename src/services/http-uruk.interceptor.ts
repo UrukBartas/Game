d070431a10714946341202e53733aaf1955994d1
Tihomir Stoychev Stoychev <tihomir_alcudia3@hotmail.com>
2024-12-04 09:37:27 +0100

    Changed way to store token, improved mine displayal and auction house actions

--- src/services/http-uruk.interceptor.ts
+++ src/services/http-uruk.interceptor.ts
@@ -1,38 +1,60 @@
 import {
-  HttpContextToken,
   HttpErrorResponse,
   HttpEvent,
   HttpHandler,
   HttpInterceptor,
-  HttpRequest,
+  HttpRequest
 } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Preferences } from '@capacitor/preferences';
 import { getNetwork } from '@wagmi/core';
-import { Observable, catchError, throwError } from 'rxjs';
+import { Observable, catchError, from, mergeMap, throwError } from 'rxjs';
+export const AUTH_TOKEN_KEY = 'auth_token';
 @Injectable()
 export class HttpUrukInterceptor implements HttpInterceptor {
   intercept(
     req: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
+    // Clonamos la solicitud para agregar o modificar los encabezados
     let request = req.clone();
-    try {
-      if (!!request.withCredentials && !!getNetwork() && !!getNetwork().chain) {
-        try {
-          request = req.clone({
-            setHeaders: {
-              chainId: getNetwork().chain.id.toString(),
-            },
-          });
-        } catch (error) {
-          console.error(error);
-        }
-      }
-    } catch (error) {
-      console.error(error);
-    }
 
-    return next.handle(request).pipe(
+    return from(Preferences.get({ key: AUTH_TOKEN_KEY })).pipe(
+      mergeMap((token) => {
+        try {
+          // Si existe el token, agregar el encabezado Authorization
+          const value = token.value;
+          if (value) {
+            request = request.clone({
+              setHeaders: {
+                Authorization: `Bearer ${value}`,
+              },
+            });
+          }
+
+          // Lógica para agregar el chainId si withCredentials es true
+          if (
+            !!request.withCredentials &&
+            !!getNetwork() &&
+            !!getNetwork().chain
+          ) {
+            try {
+              request = request.clone({
+                setHeaders: {
+                  chainId: getNetwork().chain.id.toString(),
+                },
+              });
+            } catch (error) {
+              console.error('Error configurando chainId:', error);
+            }
+          }
+        } catch (error) {
+          console.error('Error procesando la solicitud:', error);
+        }
+
+        // Continuar con la solicitud modificada
+        return next.handle(request);
+      }),
       catchError((error: HttpErrorResponse) => {
         console.error('Error HTTP:', error);
         return throwError(() => error);
