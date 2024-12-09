c7ec6e3a3483ef12033dc8e71838c87846fc379c
chmolto <chfus@proton.me>
2024-12-09 20:27:46 +0100

    feat(pvp): fix pvp result retrigger + better UI

--- src/modules/game/activities/pvp/pvp-result/pvp-result.component.ts
+++ src/modules/game/activities/pvp/pvp-result/pvp-result.component.ts
@@ -1,14 +1,30 @@
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
+import { RouteTrackingService } from 'src/modules/core/services/route-tracking.service';
 import { PvPFightService } from 'src/services/pvp-fight.service';
 
 @Component({
   selector: 'app-pvp-result',
   templateUrl: './pvp-result.component.html',
-  styleUrl: './pvp-result.component.scss',
+  styleUrls: ['./pvp-result.component.scss'],
 })
 export class PvPResultComponent {
   public fightService = inject(PvPFightService);
   public router = inject(Router);
+  public routeTrackingService = inject(RouteTrackingService);
   public getLastMatch$ = this.fightService.getLastMatch();
+  private allowedRoutes = ['/arena/auto', '/arena/pvp'];
+
+  constructor() {
+    this.handleRouting();
+  }
+
+  private handleRouting(): void {
+    console.log(this.router);
+    
+    const lastRoute = this.routeTrackingService.getPreviousUrl();
+    if (!this.allowedRoutes.includes(lastRoute)) {
+      this.router.navigate(['/leaderboard']);
+    }
+  }
 }
