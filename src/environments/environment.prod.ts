437be7294d75f73f7515f0b664b90ab9a6057ea8
Tihomir Stoychev Stoychev <tihomir_alcudia3@hotmail.com>
2025-01-15 19:27:00 +0100

    changed apiURL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   permaLinkImgPref:
     'https://raw.githubusercontent.com/UrukBartas/assets/refs/heads/main',
-  apiUrl: 'https://api-test-realm.urukbartas.com',
+  apiUrl: 'https://api.urukbartas.com',
 };
