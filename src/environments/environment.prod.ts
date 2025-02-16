c875ea83a825d6b10038b75cff44f464e146e8f0
Tihomir Stoychev Stoychev <tihomir_alcudia3@hotmail.com>
2025-02-16 21:10:58 +0100

    changed env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,5 +4,5 @@ export const environment = {
     'https://raw.githubusercontent.com/UrukBartas/assets/refs/heads/main',
   permaLinkImgPrefMobile:
     'https://raw.githubusercontent.com/UrukBartas/assets/refs/heads/main/assets_mobile',
-  apiUrl: 'https://api-test-realm.urukbartas.com/',
+  apiUrl: 'https://api-test-realm.urukbartas.com',
 };
