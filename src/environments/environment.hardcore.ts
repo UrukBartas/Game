cf4b318e52ed4ee848547133c49bc98435136776
Tihomir Stoychev Stoychev <tihomir_alcudia3@hotmail.com>
2025-04-09 17:48:53 +0200

    Updated environments

---
+++ src/environments/environment.hardcore.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  permaLinkImgPref:
+    'https://raw.githubusercontent.com/UrukBartas/assets/refs/heads/main',
+  permaLinkImgPrefMobile:
+    'https://raw.githubusercontent.com/UrukBartas/assets/refs/heads/main/assets_mobile',
+  apiUrl: 'https://hardcore-api.urukbartas.com',
+};
---
+++ src/environments/environment.test.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  permaLinkImgPref:
+    'https://raw.githubusercontent.com/UrukBartas/assets/refs/heads/main',
+  permaLinkImgPrefMobile:
+    'https://raw.githubusercontent.com/UrukBartas/assets/refs/heads/main/assets_mobile',
+  apiUrl: 'https://api-test-realm.urukbartas.com',
+};
